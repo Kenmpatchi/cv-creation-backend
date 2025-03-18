ee45f68b30333df2caad0c1a4df53aa94b3b719c
kenmpatchi <bhamzoune1920@gmail.com>
2025-03-19 00:52:52 +0100

    update

--- controllers/Caccount.js
+++ controllers/Caccount.js
@@ -21,7 +21,7 @@ exports.login=async(req,res)=>{
     const {email, password} = req.body;
     const user = await User.findOne({email});
     if(!user) return res.status(404).json({error: 'User not found!'});
-    const isMatch = bcrypt.compare(password, user.password);
+    const isMatch = await bcrypt.compare(password, user.password);
     if(!isMatch) return res.status(401).json({error: 'Invalid Password!'});
     const accessToken = jwt.sign({ id: user._id, email: user.email,phone:user.phone}, process.env.Access_Token);
     res.status(201).json({mytoken:accessToken,id:user._id});
